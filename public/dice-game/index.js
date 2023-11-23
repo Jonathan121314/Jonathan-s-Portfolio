de1eb4282a74620f81686a0b6f057490711fc65e
Jonathan121314 <pm_spam@refuge.pm>
2023-11-23 08:53:24 -0500

    added dice game and drum kit, and modified them to my needs

--- public/dice-game/index.js
+++ public/dice-game/index.js
@@ -1,18 +1,25 @@
-var player1Dice = document.querySelector(".img1");
-var player2Dice = document.querySelector(".img2");
-var heading = document.querySelector("h1");
+var rollButton = document.querySelector(".roll");
 
-var player1Number = Math.floor((Math.random() * 6) + 1);
-var player2Number = Math.floor((Math.random() * 6) + 1);
+rollButton.addEventListener("click", roll);
 
-player1Dice.setAttribute("src", "./images/dice" + player1Number + ".png");
-player2Dice.setAttribute("src", "./images/dice" + player2Number + ".png");
 
-if (player1Number > player2Number) {
-    heading.innerHTML = "🚩 Player one wins!";
-} else if (player2Number > player1Number) {
-    heading.innerHTML = "Player two wins! 🚩";
-} else {
-    heading.innerHTML = "Draw!";
+function roll() {
+    var player1Dice = document.querySelector(".img1");
+    var player2Dice = document.querySelector(".img2");
+    var heading = document.querySelector("h1");
+
+    var player1Number = Math.floor((Math.random() * 6) + 1);
+    var player2Number = Math.floor((Math.random() * 6) + 1);
+
+    player1Dice.setAttribute("src", "./images/dice" + player1Number + ".png");
+    player2Dice.setAttribute("src", "./images/dice" + player2Number + ".png");
+
+    if (player1Number > player2Number) {
+        heading.innerHTML = "🚩 Player one wins!";
+    } else if (player2Number > player1Number) {
+        heading.innerHTML = "Player two wins! 🚩";
+    } else {
+        heading.innerHTML = "Draw!";
+    }
+
 }
-
